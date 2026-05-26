eea56d3d2e547b570be8b968501ecd19c3fa29a5
kavyafullstack1-blip <kavya.fullstack1@gmail.com>
2026-05-26 06:55:39 +0530

    first commit

---
+++ day1.js
@@ -0,0 +1,49 @@
+// String
+let color = "Yellow";
+let lastName = "Allapur";
+console.log(color);
+console.log(lastName);
+
+// Number
+let length = 16;
+let weight = 7.5;
+
+// BigInt
+let x = 1234567890123456789012345n;
+let y = BigInt(1234567890123456789012345)
+
+// Boolean
+let a =true;//true;
+let b =false;//false;
+
+console.log(a);
+console.log(b);
+
+// Object Non-primitive
+const person = {firstName:"Kavya", lastName:"chennaveere"};
+console.log(person.firstName);
+console.log(person.lastName);
+
+// Array object Non-primitive
+const cars = ["maruti", "suzuki", "BMW"];
+console.log(cars[0]);
+console.log(cars[1]);
+console.log(cars[2]);
+
+// Date object Non-primitive
+const date = new Date("2026-05-26");
+console.log(date);
+
+// Undefined primitives
+let e;
+let f;
+
+// Null primitive
+let g = null;
+let h = null;
+
+// Symbol
+const i = Symbol();
+const j = Symbol();
+console.log(i);
+console.log(j);
